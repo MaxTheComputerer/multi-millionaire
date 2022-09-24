efe1ca38cbf0a5d4bf75c058c406654bb28d6f42
Max Johnson <maxelliottjohnson@hotmail.co.uk>
2022-09-24 02:03:08 +0100

    Player list and refactoring

---
+++ MultiMillionaire/TagHelpers/PlayerListItemTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MultiMillionaire.TagHelpers;
+
+public class PlayerLiTagHelper : TagHelper
+{
+    public string Id { get; set; } = string.Empty;
+    public string Class { get; set; } = string.Empty;
+    public string RightText { get; set; } = string.Empty;
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = null;
+        output.PreContent.SetHtmlContent(
+            $@"<div class=""row mb-2 mx-0 player-list {Class}"">
+                <hr class=""col-1 question-line my-auto px-0""/>
+                <div class=""col question"" id=""{Id}"">
+                    <span class=""answer-diamond text-orange me-2"">◆</span>
+                    <span>");
+        output.PostContent.SetHtmlContent(
+            $@"</span>
+            <span class=""ms-auto me-1"">{RightText}</span>
+            <span class=""answer-diamond text-orange"">◆</span>
+        </div>
+        <hr class=""col-1 question-line my-auto px-0""/>
+        </div>");
+    }
+}
--- MultiMillionaire/TagHelpers/QuestionTagHelper.cs
+++ MultiMillionaire/TagHelpers/QuestionTagHelper.cs
@@ -6,14 +6,15 @@ public class QuestionTagHelper : TagHelper
 {
     public string Id { get; set; } = string.Empty;
     public string Class { get; set; } = string.Empty;
+    public string RowClass { get; set; } = string.Empty;
 
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
         output.TagName = null;
         output.PreContent.SetHtmlContent(
-            $@"<div class=""row mb-2 {Class}"">
+            $@"<div class=""row mb-2 {RowClass}"">
                 <hr class=""col question-line my-auto px-0""/>
-                <div class=""col-md-auto question"" id=""{Id}"">");
+                <div class=""col-md-auto question {Class}"" id=""{Id}"">");
         output.PostContent.SetHtmlContent(
             @"</div>
                 <hr class=""col question-line my-auto px-0""/>
