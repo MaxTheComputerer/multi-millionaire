87d939733470736a6d3971df0b9b82dc97defed0
Max Johnson <maxelliottjohnson@hotmail.co.uk>
2022-10-19 16:51:12 +0100

    Refactor round javascript into separate files

---
+++ MultiMillionaire/wwwroot/js/fastestFinger.js
@@ -0,0 +1,132 @@
+﻿const fastestFinger = {
+    request: async () => await connection.invoke("RequestFastestFinger"),
+
+    fetchQuestion: async () => await connection.invoke("FetchFastestFingerQuestion"),
+
+    showAnswers: answers => {
+        setAnswerText("answerA", answers.A);
+        setAnswerText("answerB", answers.B);
+        setAnswerText("answerC", answers.C);
+        setAnswerText("answerD", answers.D);
+    },
+
+    onStart: function () {
+        this.input.unlock();
+        this.startTime = Date.now();
+    },
+
+    submit: async function () {
+        if (this.input.cursor === 4) {
+            const endTime = Date.now();
+            this.input.lock();
+            const duration = (endTime - this.startTime) / 1000.0;
+
+            const element = document.getElementById("fastestFingerInput");
+            const inputs = element.querySelectorAll(".fff-answer-input span");
+            const answerOrder = Array.from(inputs).map(i => i.textContent);
+
+            await connection.invoke("SubmitFastestFingerAnswer", answerOrder, duration);
+        }
+    },
+
+    revealAnswer: (index, letter, answer) => {
+        const rowElement = document.getElementById(`fffAnswer${index}`);
+        const letterElement = rowElement.querySelector(".answer-letter");
+        const textElement = rowElement.querySelector(".answer-text");
+        letterElement.textContent = `${letter}:`;
+        textElement.textContent = answer;
+        rowElement.classList.add("fff-slide-in");
+    },
+
+    populateResultsPanel: playerList => {
+        const panel = document.getElementById("fffResultsPanel");
+        for (const player of playerList) {
+            const listElement = players.listPanel.createListElement(player.connectionId, player.name.toUpperCase(), "", "", "", "fff-results-list-element_");
+            panel.appendChild(listElement);
+        }
+    },
+
+    revealCorrectPlayers: async correctPlayerTimes => {
+        for (const playerId of Object.keys(correctPlayerTimes).slice().reverse()) {
+            const listElement = document.getElementById(`fff-results-list-element_${playerId}`);
+            const rightText = listElement.querySelector(".player-list-right");
+            listElement.classList.add("correct");
+            rightText.textContent = correctPlayerTimes[playerId].toFixed(2);
+            await sleep(250);
+        }
+    },
+
+    highlightWinner: async winner => {
+        await flash(`fff-results-list-element_${winner}`, 8, 115, true);
+    },
+
+    reset: function () {
+        const resultsPanel = document.getElementById("fffResultsPanel");
+        resultsPanel.replaceChildren();
+
+        for (let i = 0; i < 4; i++) {
+            const rowElement = document.getElementById(`fffAnswer${i}`);
+            const letterElement = rowElement.querySelector(".answer-letter");
+            const textElement = rowElement.querySelector(".answer-text");
+            letterElement.textContent = "";
+            textElement.textContent = "";
+            rowElement.classList.remove("fff-slide-in");
+        }
+    },
+
+    input: {
+        cursor: 0,
+
+        unlock: () => {
+            enable("answerA");
+            enable("answerB");
+            enable("answerC");
+            enable("answerD");
+            enable("fffDeleteBtn");
+            enable("fffSubmitBtn");
+        },
+
+        lock: () => {
+            disable("answerA");
+            disable("answerB");
+            disable("answerC");
+            disable("answerD");
+            disable("fffDeleteBtn");
+            disable("fffSubmitBtn");
+        },
+
+        insert: function (letter) {
+            if (this.cursor < 4) {
+                setText(`fffInput${this.cursor++}`, letter);
+                disable(`answer${letter}`);
+            }
+        },
+
+        delete: function () {
+            if (this.cursor > 0) {
+                const id = `fffInput${--this.cursor}`;
+                const element = document.getElementById(id);
+                const letter = element.textContent;
+                setText(id, "\u{2002}");
+                enable(`answer${letter}`);
+            }
+        },
+
+        reset: function () {
+            while (this.cursor > 0) {
+                this.delete();
+            }
+            this.lock();
+        }
+    }
+}
+
+connection.on("StartFastestFinger", fastestFinger.showAnswers);
+connection.on("EnableFastestFingerAnswering", fastestFinger.onStart.bind(fastestFinger));
+connection.on("DisableFastestFingerAnswering", fastestFinger.input.lock);
+connection.on("ShowFastestFingerAnswer", fastestFinger.revealAnswer);
+connection.on("PopulateFastestFingerResults", fastestFinger.populateResultsPanel);
+connection.on("RevealCorrectFastestFingerPlayers", fastestFinger.revealCorrectPlayers);
+connection.on("HighlightFastestFingerWinner", fastestFinger.highlightWinner);
+connection.on("ResetFastestFinger", fastestFinger.reset.bind(fastestFinger));
+connection.on("ResetFastestFingerInput", fastestFinger.input.reset.bind(fastestFinger.input));
---
+++ MultiMillionaire/wwwroot/js/millionaire.js
@@ -0,0 +1,115 @@
+﻿const millionaire = {
+    request: async () => await connection.invoke("RequestMainGame"),
+
+    noNextPlayer: players => {
+        const choosePlayerSelect = document.getElementById("choosePlayerSelect");
+        choosePlayerSelect.querySelectorAll("[value]:not([value=''])").forEach(e => e.remove());
+
+        for (const player of players) {
+            const optionElement = document.createElement("option");
+            optionElement.value = player.connectionId;
+            optionElement.textContent = player.name;
+            choosePlayerSelect.appendChild(optionElement);
+        }
+
+        modals.choosePlayerModal.show();
+    },
+
+    setPlayerAndStart: async () => {
+        const choosePlayerSelect = document.getElementById("choosePlayerSelect");
+        if (choosePlayerSelect.value !== "") {
+            await connection.invoke("SetPlayerAndStart", choosePlayerSelect.value);
+        }
+    },
+
+    letsPlay: async () => await connection.invoke("LetsPlay"),
+
+    answers: {
+        submit: async letter => await connection.invoke("SubmitAnswer", letter),
+
+        select: letter => {
+            const answerElement = document.getElementById(`answer${letter}`);
+            answerElement.classList.add("selected");
+        },
+
+        highlightCorrect: async letter => {
+            const answerElement = document.getElementById(`answer${letter}`);
+            answerElement.classList.add("correct");
+        },
+
+        flashCorrect: async letter => {
+            await flash(`answer${letter}`);
+        },
+
+        resetBackgrounds: () => {
+            ["answerA", "answerB", "answerC", "answerD"].forEach(id => {
+                const element = document.getElementById(id);
+                element.classList.remove("correct", "selected");
+            })
+        }
+    },
+
+    banners: {
+        winnings: {
+            show: amount => {
+                const winningsRow = document.getElementById("winnings");
+                winningsRow.querySelector(".winnings-text").textContent = amount;
+                hide("questionAndAnswers");
+                show("winnings", "flex");
+            },
+
+            hide: () => {
+                hide("winnings");
+                show("questionAndAnswers");
+            },
+        },
+
+        showTotalPrize: amount => {
+            const prizeRow = document.getElementById("totalPrize");
+            prizeRow.querySelector(".total-prize-text").textContent = amount;
+            hide("questionAndAnswers");
+            show("totalPrize", "flex");
+        },
+
+        showMillionaire: name => {
+            const prizeRow = document.getElementById("millionairePrize");
+            prizeRow.querySelector(".millionaire-prize-text").textContent = name.toUpperCase();
+            hide("questionAndAnswers");
+            show("millionairePrize", "flex");
+        }
+    },
+
+    moneyTree: {
+        set: questionNumber => {
+            if (questionNumber > 1) {
+                const previousRow = document.getElementById(`tree-${questionNumber - 1}`);
+                previousRow.classList.remove("tree-selected");
+            }
+
+            const currentRow = document.getElementById(`tree-${questionNumber}`);
+            currentRow.classList.add("tree-selected");
+        },
+
+        reset: () => {
+            const elements = document.querySelectorAll(".tree-selected");
+            elements.forEach(e => e.classList.remove("tree-selected"));
+        }
+    },
+
+    walkAway: async () => await connection.invoke("WalkAway"),
+
+    lifelines: {}
+}
+
+connection.on("NoNextPlayer", millionaire.noNextPlayer);
+connection.on("DismissChoosePlayerModal", modals.choosePlayerModal.hide);
+connection.on("SelectAnswer", millionaire.answers.select);
+connection.on("HighlightCorrectAnswer", millionaire.answers.highlightCorrect);
+connection.on("FlashCorrectAnswer", millionaire.answers.flashCorrect);
+connection.on("ShowWinnings", millionaire.banners.winnings.show);
+connection.on("HideWinnings", millionaire.banners.winnings.hide);
+connection.on("ShowTotalPrize", millionaire.banners.showTotalPrize);
+connection.on("ShowMillionaireBanner", millionaire.banners.showMillionaire);
+connection.on("SetMoneyTree", millionaire.moneyTree.set);
+connection.on("ResetMoneyTree", millionaire.moneyTree.reset);
+connection.on("ResetAnswerBackgrounds", millionaire.answers.resetBackgrounds);
